43b9ad0205247e81ea1de3774c21f04508d01596
kabust <tulyulyuk91@gmail.com>
2024-04-12 11:08:08 +0200

    fixed async execution

--- app/main.py
+++ app/main.py
@@ -23,15 +23,16 @@ async def main() -> None:
     )
 
     await run_parallel(
-        run_sequence(
-            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
-            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
-        ),
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
         run_sequence(
             service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
             service.send_msg(Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")),
-            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
-        ),
+        )
+    )
+
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
         run_sequence(
             service.send_msg(Message(toilet_id, MessageType.FLUSH)),
             service.send_msg(Message(toilet_id, MessageType.CLEAN)),
